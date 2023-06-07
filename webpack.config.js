2435cdc36626091ab0dbf2da632b0bbd16dc1215
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-06-06 20:24:04 -0600

    setup local dev server

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,23 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode:'production',
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
         template:'src/index.html',
     })
   ],
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
   module: {
     rules: [
