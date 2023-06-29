22b65eec8b12ed12e8d448f37d23537aae8c29e6
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-06-29 15:06:36 -0600

    add some test to check if jest and jsdom is working properly

--- src/index.test.js
+++ src/index.test.js
@@ -2,3 +2,29 @@
  * @jest-environment jsdom
  */
 
+// math.js
+function add(a, b) {
+    return a + b;
+  }
+  
+  // math.test.js
+  test('add function adds two numbers correctly', () => {
+    // Arrange
+    const a = 2;
+    const b = 3;
+  
+    // Act
+    const result = add(a, b);
+  
+    // Assert
+    expect(result).toBe(5);
+  });
+
+
+  //lets test if we can work with DOM
+  const container = document.createElement('ul');
+  container.innerHTML = `
+  <li></li>
+  <li></li>
+  `
+  console.log(container);
